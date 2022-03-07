a2109635759590855a0e50c5343169c13518f48e
kamil-ciesla <kamil.ciesla.dev@gmail.com>
2022-03-07 19:10:57 +0100

    reworked server code, used  expressjs

--- server/index.js
+++ server/index.js
@@ -1,24 +1,39 @@
-//Load HTTP module
-const http = require("http");
-const hostname = "127.0.0.1";
-const port = 3000;
-
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
-	//Set the response HTTP header with HTTP status and Content type
-	res.statusCode = 200;
-	res.setHeader("Content-Type", "text/plain");
-	res.end("Hello World\n");
-});
-
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
-});
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const port = 3000
 
 const ytdl = require("ytdl-core");
 const fs = require("fs");
 
-ytdl("http://www.youtube.com/watch?v=aqz-KE-bpKQ").pipe(
-	fs.createWriteStream("video.mp4")
-);
+// We are using our packages here
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+	extended: true
+}));
+app.use(cors())
+
+//You can use this to check if your server is working
+app.get('/', (req, res) => {
+	res.send("Welcome to your server")
+})
+
+//Route that handles login logic
+app.post('/convert', (req, res) => {
+	console.log(req.body.link)
+	ytdl(req.body.link).pipe(
+		fs.createWriteStream("video.mp4")
+	);
+	res.send('Video is being downloaded on server! ');
+})
+
+
+//Start your server on a specified port
+app.listen(port, () => {
+	console.log(`Server is runing on port ${port}`)
+})
+
+
+
