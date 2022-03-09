3e36767593881e88290dce6506f052ba2014f9de
kamil-ciesla <kamil.ciesla.dev@gmail.com>
2022-03-09 12:40:56 +0100

    new version for akayee

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const port = 3000
-
+const ffmpeg = require('fluent-ffmpeg');
 const ytdl = require("ytdl-core");
+const fileUpload = require('express-fileupload');
 const fs = require("fs");
 
+const port = 3000
+const mp4Path = './downloaded/video.mp4';
+const mp3Path = './downloaded/audio.mp3';
 // We are using our packages here
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 
@@ -14,20 +17,45 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 	extended: true
 }));
 app.use(cors())
+app.use(express.static(__dirname + '/public'));
+app.use(fileUpload({
+	useTempFiles: true,
+	tempFileDir: "/tmp/"
+}))
 
 //You can use this to check if your server is working
 app.get('/', (req, res) => {
-	res.send("Welcome to your server")
+	res.sendFile(__dirname + '/index.html');
 })
 
-//Route that handles login logic
-app.post('/convert', (req, res) => {
-	console.log(req.body.data)
-	ytdl(req.body.data).pipe(
-		fs.createWriteStream("./downloaded/video.mp4")
-	);
+function youtubeDownload(req) {
+	return new Promise((resolve, reject) => {
+		resolve(ytdl(req.body.data).pipe(
+			fs.createWriteStream("./downloaded/video.mp4")
+		))
+	})
+}
+
+app.post('/convert', async (req, res) => {
+	await youtubeDownload(req);
+
+	proc = new ffmpeg({ source: mp4Path });
+	proc.setFfmpegPath('C:/ffmpeg/bin/ffmpeg.exe');
+	proc.saveToFile(mp3Path, (stdout, stderr) => {
+		return stderr ? console.log(stderr) : console.log('Video is being converted to mp3.');
+	}
+	)
 	res.send(JSON.stringify({ x: "foo" }));
 })
+
+function convertToMp3() {
+	proc = new ffmpeg({ source: mp4Path });
+	proc.setFfmpegPath('//');
+	proc.saveToFile(mp3Path, (stdout, stderr) => {
+		return stderr ? console.log(stderr) : console.log('Video is being converted to mp3.');
+	}
+	)
+}
 
 app.get('/download', (req, res) => res.download('./downloaded/video.mp4'))
 //Start your server on a specified port
