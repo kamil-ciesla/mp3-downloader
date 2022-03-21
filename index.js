3c7ff0b9845553d7aa0d3c512fb4cbf6f25fb70a
kamil-ciesla <kamil.ciesla.dev@gmail.com>
2022-03-21 20:07:38 +0100

    preparations for vercel deployment

--- index.js
+++ index.js
@@ -2,62 +2,19 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const ffmpeg = require('fluent-ffmpeg');
-const ytdl = require("ytdl-core");
-const fileUpload = require('express-fileupload');
-const fs = require("fs");
-const { ReadStream } = require('tty');
+const downloader = require('./api/downloader');
 
-const port = 3000
-const mp4Path = './downloaded/video.mp4';
-const mp3Path = './downloaded/audio.mp3';
-// We are using our packages here
+const port = process.env.PORT || 3000
+
+app.use('/api/downloader', downloader);
+
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 	extended: true
 }));
 app.use(cors())
-app.use(express.static(__dirname + '/public'));
-app.use(fileUpload({
-	useTempFiles: true,
-	tempFileDir: "/tmp/"
-}))
 
-//You can use this to check if your server is working
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
-})
-
-function downloadVideo(link) {
-	return new Promise((resolve, reject) => {
-		const stream = ytdl(link);
-		// Version for converting using file.
-		//stream.pipe(fs.createWriteStream(mp4Path)).on('finish', () => { resolve(stream); });
-		// Version for converting using stream.
-		stream.pipe(fs.createWriteStream(mp4Path));
-		resolve(stream);
-
-	})
-}
-
-function convertToMp3(stream) {
-	proc = new ffmpeg({ source: stream });
-	//proc = new ffmpeg({ source: stream });
-	proc.setFfmpegPath('C:/ffmpeg/bin/ffmpeg.exe');
-	proc.saveToFile(mp3Path, (stdout, stderr) => {
-		return stderr ? console.log(stderr) : console.log('Video is being converted to mp3.');
-	}
-	)
-}
-app.post('/convert', async (req, res) => {
-	const link = req.body.data;
-	const stream = await downloadVideo(link);
-	convertToMp3(stream);
-})
-
-
-app.get('/download', (req, res) => res.download('./downloaded/video.mp4'))
 //Start your server on a specified port
 app.listen(port, () => {
 	console.log(`Server is runing on port ${port}`)
